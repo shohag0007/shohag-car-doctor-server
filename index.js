230cf1a87cc80a0a22a4f13ca2ea2003648c5fc7
shohag0007 <merinedrive007@gmail.com>
2024-08-01 01:17:35 +0600

    shohagCar is done with crud operations

--- index.js
+++ index.js
@@ -41,15 +41,51 @@ async function run() {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) }
         const options = {
-            projection: { title: 1, price: 1, service_id: 1}
+            projection: { title: 1, price: 1, service_id: 1, img: 1, name: 1 }
         }
         const result = await serviceCollection.findOne(query, options);
         res.send(result);
      })
     //bookings
      
+    app.get('/bookings', async(req, res) =>{
+      console.log(req.query.email);
+      let query = {};
+      if(req.query?.email){
+        query = {email: req.query.email}
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    })
+
+
     app.post('/bookings', async (req, res) => {
       const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.patch('/bookings/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+
+      const updateDoc = {
+        $set: {
+          status: updatedBooking.status
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    app.delete('/bookings/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
     })
 
 
